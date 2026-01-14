0bd5b6e41aa42c2abe7219a9ef20414fb333a82d
Obsidian AJ <ajtakesthecake@gmail.com>
2026-01-14 09:40:47 -0600

    planet_shape

--- game.py
+++ game.py
@@ -1,6 +1,40 @@
 import pygame
 import sys
 
+class Planet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.radius = 50
+        # self.image = pygame.Surface((self.radius * 3, self.radius * 3), pygame.SRCALPHA)
+        # pygame.draw.circle(self.image, (0,0,150), (self.radius, self.radius), self.radius)
+        self.image = pygame.Surface((self.radius * 2, self.radius * 2), pygame.SRCALPHA)
+        pygame.draw.circle(self.image, BLUE, (self.radius, self.radius), self.radius)
+        self.rect = self.image.get_rect(topleft = (x,y))
+        
+    def gravity(self):
+        pass
+        
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y, color):
+        super().__init__()
+        self.image = pygame.Surface((40, 40))
+        self.image.fill(color)
+        self.rect = self.image.get_rect(topleft = (x,y))
+
+    def move(self, deltax, deltay):
+        if self.rect.left < 0 or self.rect.right>1200:
+            deltax *= -3
+        if self.rect.top < 0 or self.rect.bottom > 600:
+            deltay *= -3
+
+        self.rect.centerx += deltax
+        self.rect.centery += deltay
+
+
+
+
+
 # Initialize Pygame and give access to all the methods in the package
 pygame.init()
 
@@ -18,6 +52,9 @@ BLACK = (60,60,60)
 # Create clock to later control frame rate
 clock = pygame.time.Clock()
 
+planets = pygame.sprite.Group()
+planets.add(Planet(300, 300))
+
 # Main game loop
 running = True
 while running:
@@ -28,6 +65,7 @@ while running:
 
     # Fill the screen with a color (e.g., white)
     screen.fill(BLACK)
+    planets.draw(screen)
 
 
 
